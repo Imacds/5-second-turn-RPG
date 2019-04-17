452a9fab64f0038521fc710a34cb445cd389a44f
Cubba Jeron <imacds@gmail.com>
2019-04-16 18:27:23 -0700

    Fixed bug involving skipping the last dialog

--- scripts/DialogBox.gd
+++ scripts/DialogBox.gd
@@ -1,31 +1,45 @@
 extends RichTextLabel
 
 # Declare member variables here. Examples:
-var dialog = ["This is 6 second RPG created by Gridventure", "Enjoy the game"]
+var dialog = ["This is 6 second RPG created by Gridventure", "Enjoy the game!"]
 var page = 0
+var isDone = false
 onready var DialogBox = get_parent()
+onready var label = DialogBox.get_node("Label")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_bbcode(dialog[page])
 	set_visible_characters(0)
 	set_process_input(true)
-	pass # Replace with function body.
+	isDone = false
+
+func _process(delta):
+	if get_visible_characters() < get_total_character_count():
+		label.text = "Press any key to skip"
+	else:
+		label.text = "Press any key to continue"
 
 func _input(event):
 	
 	if event is InputEventKey or event is InputEventMouseButton:
 		if event.pressed:
-			if page < len(dialog)-1:
+			if isDone:
+				DialogBox.visible = false
+			else:
 				if get_visible_characters() > get_total_character_count():
 					if page < dialog.size() - 1:
 						page += 1
 						set_bbcode(dialog[page])
 						set_visible_characters(0)
+					else:
+						isDone = true
 				else:
 					set_visible_characters(get_total_character_count())
-			else:
-				DialogBox.visible = false
 
 func _on_Timer_timeout():
 	set_visible_characters(get_visible_characters() + 1)
-	pass # Replace with function body.
+	
+	if get_visible_characters() > get_total_character_count():
+		if page >= dialog.size() - 1:
+			isDone = true

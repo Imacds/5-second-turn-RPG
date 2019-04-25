4d56ef1231cd19edda422c6d252c7380f887c074
James T <jamest@earny.co>
2019-04-25 12:39:59 -0700

    rm: unused code in Player.gd and TurnManager.gd refactor: a bunch of attributes for player and editor vars for player

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -3,7 +3,9 @@ extends KinematicBody2D
 ###################
 # editor vars #
 ###################
-export(float) var SPEED = 300.0
+export(float) var speed = 300
+export(float) var max_speed = 400
+export(float) var mass = 10.0
 export(int) var hp = 4
 export(int) var walk_distance = 3
 export(int) var action_points_per_turn = 3
@@ -37,35 +39,19 @@ var AttackAction = load("res://scripts/AgentActionSystem/AttackAction.gd")
 var WaitAction = load("res://scripts/AgentActionSystem/WaitAction.gd")
 
 var _state = STATES.TURN
-var action_points = 3
-var is_attack_turn = false
-var path = []
-var target_point_world = position
-var target_position = Vector2()
-var attack_mode = null # describes the type of attack if any. value from enum from AttackTemplate.gd
 var command_mode = COMMAND_MODES.NULL # indicates allowed input reading for this player controlled character
+var action_points = 3
+var target_point_world = position
+var attack_mode = null # describes the type of attack if any. value from enum from AttackTemplate.gd
 var attack_dir = Vector2.LEFT
-var direction = Vector2()
 
 onready var Finder = get_node("/root/ObjectFinder")
 
 onready var attack_template = get_tree().get_root().get_node("Root/AttackTemplate")
 onready var selection_manager = get_tree().get_root().get_node("Root/SelectionManager")
-onready var turn_manager = get_tree().get_root().get_node("Root/TurnManager")
 onready var map = get_tree().get_root().get_node("Root/Map")
 onready var attack_map = Finder.get_node_from_root("Root/AttackMap")
-onready var pathing = get_parent().get_node("Path")
 onready var action_queue = $ActionQueue
-
-var dragging = false
-
-var is_moving = false
-var target_pos = position
-var character_target_position = position
-var target_dir = Vector2()
-
-var speed = 0
-const max_speed = 400
 
 var velocity = Vector2()
 
@@ -81,23 +67,8 @@ func _ready():
 
 func get_cell_coords():
 	return map.world_to_map(position)
-	# get world position, size of individual cell, divide & truncate
-#	var x = int(position.x / map.cell_size.x)
-#	var y = int(position.y / map.cell_size.y)
-#	return Vector2(x, y)
-	
 
 func _change_state(new_state):
-#	if  new_state == STATES.IDLE:
-#		emit_signal("agent_exits_walk_mode", get_cell_coords())
-#	if new_state == STATES.WAIT:
-#		path = pathing.get_path_relative(position, target_position)
-#		if not path or len(path) == 1:
-#			_change_state(STATES.IDLE)
-#			return
-#		# The index 0 is the starting cell
-#		# we don't want the character to move back to it in this example
-#		target_point_world = path[1]
 	_state = new_state
 	
 func _change_command_mode(new_mode):
@@ -131,83 +102,25 @@ func render_hp():
 
 func _process(delta):
 	render_hp()
-	
-#	if _state != STATES.TURN:
-#		return
-	
-#	if attack_mode != null and (attack_template.click_mode == null or selection_manager.selected == get_parent()):
-	if can_attack():
-		pass
-		
-	if attack_mode != null and can_attack():
-		pass
-#		var dir_str = $Attack.get_attack_dir_str($Attack.get_relative_attack_dir())
-#		attack_template.do_attack(get_cell_coords(), attack_mode, dir_str, self)
-#		attack_mode = null
-#		_change_state(STATES.IDLE)
-#		_change_command_mode(COMMAND_MODES.MOVE)
-	else:
-#		var arrived_to_next_point = move_to(target_point_world)
-#		if arrived_to_next_point:
-#			_change_state(STATES.WAIT)
-#			path.remove(0)
-#			if len(path) == 0:
-#				_change_state(STATES.IDLE)
-#				return
-#			target_point_world = path[0]
-		pass
-
 
 func _physics_process(delta):
-	move()
-#	if character_target_position != position:
-#		move_to(target_pos)
-#	if _state != STATES.TURN and _state != STATES.IDLE:
-		
-#		var arrived_to_next_point = move_to(target_point_world)
-#		if arrived_to_next_point:
-#			_change_state(STATES.WAIT)
-#			path.remove(0)
-#			if len(path) == 0:
-#				_change_state(STATES.IDLE)
-#				return
-#			target_point_world = path[0]
+	_move()
 
-
-func move():
+func _move():
 	if _state != STATES.TURN and _state != STATES.IDLE: # if we're not waiting for the other player to make their move and we're not in "not moving" state waiting for command input
-		var arrived_to_next_point = move_to(target_point_world)
-		if arrived_to_next_point:
-			path.remove(0)
-
-			position = target_point_world
-			if len(path) == 0:
-				_change_state(STATES.IDLE)
-				return
+		if move_to(target_point_world):
 			_change_state(STATES.WAIT)
-			target_point_world = path[0]
 			emit_signal("single_action_finished", MoveAction.get_name())
-		
-
-func do_turn(is_attack_turn):	
-	self.is_attack_turn = is_attack_turn
-	if _state == STATES.WAIT or attack_mode != null:
-		_state = STATES.TURN
-	else:
-		attack_map.clear()
-
 
 func move_to(world_position):
-	var MASS = 10.0
-	var ARRIVE_DISTANCE = 0
+	var arrive_distance = 0
 
-	var desired_velocity = (world_position - position).normalized() * SPEED
+	var desired_velocity = (world_position - position).normalized() * speed
 	var steering = desired_velocity - velocity
-	velocity += steering / MASS
+	velocity += steering / mass
 	position += velocity * get_process_delta_time()
 	
-	return position.distance_to(world_position) <= ARRIVE_DISTANCE
-
+	return position.distance_to(world_position) <= arrive_distance
 
 func move_one_cell(direction):
 	_change_state(STATES.WAIT)
@@ -220,17 +133,8 @@ func move_one_cell(direction):
 	# calc world destination
 	var world_destination = map.map_to_world(coords, false) + map._half_cell_size # todo: why is this method giving the wrong world coords?
 	target_point_world = world_destination
-	path = [position, world_destination]
 
 	var arrived = move_to(world_destination)
-	
-
-#func _unhandled_input(event):
-#	if event.is_action_pressed("click"):
-#		if can_move(): 
-#			_change_command_mode(COMMAND_MODES.MOVE)
-#			action_points -= 1
-#			$ActionQueue.push(MoveAction.new(self, Vector2.RIGHT))
 
 func preview_attack(attack_template_attack_mode, dir_str, tile_type):
 	attack_template.visualize_attack(get_cell_coords(), attack_template_attack_mode, dir_str, self, tile_type) # position, attack_mode, attack_dir, owner, tile_type
@@ -262,8 +166,8 @@ func queue_attack_action(attack_matrix):
 	attack_template.visualize_attack(get_cell_coords(), attack_mode, dir_str, self, attack_map.TILES.YELLOW_ZONE_TO_ATTACK) # position, attack_mode, attack_dir, owner, tile_type
 	
 	attack_template.set_click_mode(null)
-	
 	var queue_had_room = $ActionQueue.push(WaitAction.new()) and $ActionQueue.push(action)
+	
 	if AttackAction.can_do_action(action, action_points) and queue_had_room:
 		action_points -= action.get_cost()
 	else: # not enough AP or queue is full
--- scripts/TurnManager.gd
+++ scripts/TurnManager.gd
@@ -7,35 +7,13 @@ onready var players = selection_manager.players
 onready var attack_map = $"../AttackMap"
 
 # used to determine when all agents have done all their actions queued
-var agents_on_map = 2 # todo: better way to do this
+var agents_on_map = 2 # todo: better way to do this - create some stat
 var agents_who_finished_actions_count = 0
 
 
 func end_turn():
 	for player in players:
 		player.get_node("Char").action_queue.execute_all(wait_time_per_tick)
-	
-#	for i in range(0, amount):
-#		#Ticks a turn
-#		attack_map.clear()
-#		do_one_turn(i == 1)
-#
-#		#Waits 3 seconds
-#		var timer = Timer.new()
-#		timer.set_wait_time(wait_time_per_tick)
-#		timer.set_one_shot(true)
-#
-#		add_child(timer)
-#		timer.start()
-#
-#		yield(timer, "timeout")
-#		timer.queue_free()
-#
-#	attack_map.clear()
-	
-func do_one_turn(is_attack_turn):
-	for player in players:
-		player.get_node("Char").do_turn(is_attack_turn)
 		
 func get_player_names():
 	return [players[0].get_name(), players[1].get_name()]
@@ -44,3 +22,4 @@ func _on_Char_action_queue_finished_executing(agent_name):
 	agents_who_finished_actions_count += 1
 	if agents_who_finished_actions_count >= agents_on_map:
 		attack_map.clear()
+		agents_who_finished_actions_count = 0

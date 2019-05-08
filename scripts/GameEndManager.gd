7d7f36d78bc080f6c741fcccc8454cc57e181b06
James T <github.jtara1@gmail.com>
2019-05-08 01:30:25 -0700

    chg: allow big flash banner to have color chagned and make sure to stop in prog anim before starting next one

--- scripts/GameEndManager.gd
+++ scripts/GameEndManager.gd
@@ -1,7 +1,8 @@
 extends Node
 
+signal game_over()
+
 onready var turn_manager = $"../TurnManager"
-#onready var agents = turn_manager.AIs + turn_manager.players
 onready var agents = turn_manager.agents
 onready var agents_alive = len(agents)
 
@@ -21,4 +22,6 @@ func _on_Char_agent_died(agent):
 	agents_alive -= 1
 	agent.disconnect("agent_died", self, "_on_Char_agent_died")
 	if only_players_remain():
-		print('victory')
+		print('vectory')
+		$"../TurnManager/TurnTimer".set_paused(true)
+		$"../LargeNotifyBanner".play_animation("Victory!", Color.green)
--- scripts/UI/LargeNotifyBanner.gd
+++ scripts/UI/LargeNotifyBanner.gd
@@ -2,7 +2,12 @@ extends ColorRect
 
 var animation_name = "TurnChangeAnim"
 
-func play_animation(text):
+func play_animation(text, bg_color = Color.black):
+	if $AnimationPlayer.is_playing():
+		$AnimationPlayer.stop()
+	
+#	set_modulate(bg_color)
+	color = bg_color
 	$TurnChangeLabel.set_text(text)
 	$AnimationPlayer.play(animation_name)
 
